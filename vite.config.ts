07212acdaa1857100504ebf0918f2cc640311925
Abhishek Tiwari <103898973+Ajoyabhi@users.noreply.github.com>
2025-09-09 00:43:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { fileURLToPath } from "url";
+import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
+// Fix __dirname in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [
     react(),
+    runtimeErrorOverlay(),
     ...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
       ? [
